374da333c457737c5d72aaf06e1c684a0ee9be2e
HarmonyClexx <harmonymupindu@gmail.com>
2024-01-17 16:14:53 +0200

    Initial commit

---
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.beeradviser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//    called when the user clicks the button
+    public void onClickFindBeer(View view){
+
+    }
+}
